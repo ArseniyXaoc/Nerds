2dd5306f07e9d6937ffaa15c773137512fbbef80
ArseniyXaoc <xaoc190789@gmail.com>
2020-05-01 16:36:12 +0300

    gulp+scss+bs

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,40 @@
 'use strict';
 
 const gulp = require('gulp'),
-      brSync = require('browser-sync').create(); // подключаем плагин browser sync 
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    brSync = require('browser-sync').create();
+
+gulp.task('sass', () => {
+    return gulp.src('app/sass/**/*.scss')
+        .pipe(sourcemaps.init())        // активируем gulp-sourcemaps 
+        .pipe(sass({
+            outputStyle: 'nested',
+        })
+            .on('error', sass.logError))
+        .pipe(sourcemaps.write())   // создание карты css.map в текущей папке 
+        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(brSync.reload({ stream: true }))
+});
 
 gulp.task('brSync', () => {
     brSync.init({
-        server: {           // локальный сервер 
-            baseDir: "app" // корневая папка 
+        server: {
+            baseDir: "dist"
         },
-        notify: false       // отклчение уведомлений 
+        notify: false
     });
 });
 
-gulp.task('html', () => {   
+gulp.task('html', () => {
     return gulp.src('app/**/*.html', { since: gulp.lastRun('html') })
-        //.pipe(gulp.dest('dist'))                // перенос HTML в папку деплоя 
-        .pipe(brSync.reload({ stream: true }))  // перезагрузка страницы 
+        .pipe(gulp.dest('dist'))
+        .pipe(brSync.reload({ stream: true }))
 });
 
 gulp.task('watch', gulp.parallel('brSync', () => {
-    gulp.watch('app/*.html', gulp.parallel('html')) // следим за изменениями HTML 
+    gulp.watch('app/sass/**/*.scss', gulp.parallel('sass')); // следим за изменениями SASS 
+    gulp.watch('app/*.html', gulp.parallel('html'))     // следим за изменениями HTML 
 }));
 
-gulp.task('default', gulp.parallel('html', 'watch')); // задача по умолчанию (gulp)
+gulp.task('default', gulp.parallel('sass', 'html', 'watch'));
